b1433d73922e1cf0e3ff0df217e025c9186d14f1
Bin Hu <elithrade@gmail.com>
2016-12-11 22:05:11 +0800

    Aliases tweak

--- aliases.sh
+++ aliases.sh
@@ -30,7 +30,7 @@ alias nrc="npm run commit";
 
 # Yaourt/pacman
 alias yao="yaourt"
-alias autoremove="sudo pacman -Rns $(pacman -Qqtd)"
+alias autoremove="sudo pacman -Rcns $(pacman -Qdtq)"
 
 # Tmux
 alias t="tmux"
